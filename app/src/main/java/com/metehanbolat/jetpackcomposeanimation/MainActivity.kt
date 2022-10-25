82827a4701e2bea1262b3b86073929e3eb4fe2cd
Metehan <metehan.bolat.ie@gmail.com>
2022-10-25 22:59:09 +0300

    Animate Visibility

--- app/src/main/java/com/metehanbolat/jetpackcomposeanimation/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/jetpackcomposeanimation/MainActivity.kt
@@ -3,6 +3,22 @@ package com.metehanbolat.jetpackcomposeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.metehanbolat.jetpackcomposeanimation.ui.theme.JetpackComposeAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +26,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeAnimationTheme {
-
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    var isVisible by remember { mutableStateOf(false) }
+                    Button(
+                        onClick = {
+                            isVisible = !isVisible
+                        }
+                    ) {
+                        Text(text = "Toggle")
+                    }
+                    AnimatedVisibility(
+                        visible = isVisible,
+                        enter = slideInHorizontally() + fadeIn(),
+                        modifier = Modifier.fillMaxWidth().weight(1f)
+                    ) {
+                        Box(modifier = Modifier.background(Color.Red))
+                    }
+                }
             }
         }
     }
